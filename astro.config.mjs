29657d4ed2bfe71319406a60ea16dd55adcd8140
Scott A. Green <sagreenxyz@gmail.com>
2026-05-03 17:38:34 -0500

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sagreenxyz.github.io',
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
